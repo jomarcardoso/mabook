20ba5c3b018cdc54d9e8304b62ca393c9091e617
Jomar <jomar.cardoso@cwi.com.br>
2021-05-11 01:08:42 -0300

    feat: 🎸 bootstrap css and js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
 
 @NgModule({
@@ -12,7 +11,6 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     BrowserModule,
-    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
